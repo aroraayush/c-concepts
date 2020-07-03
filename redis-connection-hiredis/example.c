6fe780535b96f469a04eeab636585e427df8d969
ayush884 <ayush@whatsbusy.com>
2020-07-02 18:10:36 -0700

    hiredis example updated

--- redis-connection-hiredis/example.c
+++ redis-connection-hiredis/example.c
@@ -1,14 +1,10 @@
-/* ------ This is a example program to connect redis with C client library (hiredis) ------ */
-
-// Complete the setup before (the README in the current directory) to run the below program
-
 #include <stdlib.h>
 
 # include "hiredis/hiredis.h"
 
-
 int main(int argc, char **argv) {
     
+    /* Connecting to redis */
     const char *hostname = (argc > 1) ? argv[1] : "127.0.0.1";
     int port = (argc > 2) ? atoi(argv[2]) : 6379;
     unsigned isunix = 0;
@@ -31,6 +27,30 @@ int main(int argc, char **argv) {
         exit(1);
     }
     printf("Connected to redis\n");
+
+    // redisFree(c);
+
+    /* 2 - PING server */
+    redisReply *reply;  /* Temporary reply pointer */
+    reply = redisCommand(c,"PING");
+    printf("PING: %s\n", reply->str);
+
+    /* 3 - Set a key */
+    reply = redisCommand(c,"SET %s %s", "foo", "hello world");
+    printf("SET %s %s \t| %s\n", "foo", "hello world", reply->str);
+    freeReplyObject(reply);     // Free a reply object
+
+    /* 3 - Get a key */
+    reply = redisCommand(c,"GET %s","foo");
+    printf("GET %s \t\t| ","foo");
+    printf("%s\n",reply->str);
+    freeReplyObject(reply);
+
+    /* Set a key using binary safe API */
+    // reply = redisCommand(c,"SET %b %b", "bar", (size_t) 3, "hello", (size_t) 5);
+    // printf("SET (binary API): %s\n", reply->str);
+    // freeReplyObject(reply);
+    redisFree(c);
+
     return 0;
 }
-
